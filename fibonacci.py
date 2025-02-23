b33fdc6da4994ac8b792ddd76c8da1ce4854723c
Sakthi Sanjana M <73226923+sanjana2804@users.noreply.github.com>
2025-02-23 13:12:30 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+n=int(input("Enter the number of terms you need :"))
+n1=0
+n2=1
+if n<=0:
+  if n<0:
+    print("the number of terms is negative")
+  else:
+    print(n2)
+else:
+  print("Fibonacci sequence : ", n1,n1,end=" ")
+  for i in range(2,n):
+    n3=n1+n2
+    n1=n2
+    n2=n3
+    print(n3, end=" ")
